47c20102cb9b58b850609a9dedf552f8c60e7f80
MAMAsoon <78579013+chayada0802@users.noreply.github.com>
2021-11-11 10:21:31 +0700

    Add files via upload

---
+++ HWQueue.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace HWQueue
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Queue CPUQueue = new Queue();
+            char instruction;
+            char data;
+            int count=0;
+            int Round=0;
+            while (true)
+            {
+                instruction = char.Parse(Console.ReadLine());
+                
+                if (instruction =='?')
+                {
+                    break;
+                }
+                data = char.Parse(Console.ReadLine());
+                Node CPU = new Node(instruction, data);
+                CPUQueue.Push(CPU);
+               
+            }
+            Node instQueue;
+            while (true)
+            {
+                instQueue = CPUQueue.Pop();
+                count++;
+                if(instQueue==null)
+                {
+                    break;
+                }
+              
+            }
+            if(count%3>0)
+            {
+                Round = count / 3+1;
+            }
+            if( count % 3 == 0)
+            {
+                Round = count / 3;
+            }
+            Console.WriteLine("CPU cycles needed:" + Round);
+
+
+
+        }
+    }
+    class Node
+    {
+        public char Instruction;
+        public char Data;
+        public Node Next;
+        public Node (char instructionValue,char dataValue)
+        {
+            Instruction = instructionValue;
+            Data = dataValue;
+        }
+        public override string ToString()
+        {
+            return String.Format("({0}, {1})", Instruction, Data);
+        }
+
+    }
+    class Queue
+    {
+        private Node Root;
+        public void Push(Node node)
+        {
+            if(Root == null)
+            {
+                Root = node;
+            }
+            else
+            {
+                Node ptr = Root;
+                while(ptr.Next != null)
+                {
+                    ptr = ptr.Next;
+                }
+                ptr.Next = node;
+            }
+        }
+        public Node Pop()
+        {
+            if(Root==null)
+            {
+                return null;
+            }
+            Node node = Root;
+            Root = Root.Next;
+            node.Next = null;
+            return node;
+        }
+        public Node Get(int index)
+        {
+            Node node = Root;
+            while (index > 0)
+            {
+                if (node == null)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                node = node.Next;
+                index--;
+            }
+            return node;
+        }
+
+    }
+}
